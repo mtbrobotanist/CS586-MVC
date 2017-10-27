5c1d7b63e1bb0e689ba5dd1d8fb99189cb311778
Unknown <floydwin7@gmail.com>
2017-10-26 23:08:31 -0700

    fixed all of the old AspNetAngular1 references again

--- CS586MVC/Controllers/SampleDataController.cs
+++ CS586MVC/Controllers/SampleDataController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace AspNetAngular1.Controllers
+namespace CS586MVC.Controllers
 {
     [Route("api/[controller]")]
     public class SampleDataController : Controller
