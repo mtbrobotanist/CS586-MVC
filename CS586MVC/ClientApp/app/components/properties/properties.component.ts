f1f16f32d1286936889bfda4369ca8acac22ad33
Unknown <floydwin7@gmail.com>
2017-11-01 01:04:23 -0700

    added Property interface

--- CS586MVC/ClientApp/app/components/properties/properties.component.ts
+++ CS586MVC/ClientApp/app/components/properties/properties.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import {Http } from "@angular/http";
 
 @Component({
   selector: 'app-properties',
   templateUrl: './properties.component.html',
   styleUrls: ['./properties.component.css']
 })
-export class PropertiesComponent implements OnInit {
+export class PropertiesComponent {
+    public properties: Property[];       
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+    constructor(http: Http, @Inject('BASE_URL') baseUrl: string) {
+        http.get(baseUrl + 'PropertyData/properties').subscribe(result => {
+            this.properties = result.json() as Property[];
+        }, error => console.error(error));
+    }
 
 }
+
+interface Property {
+id:number;
+address:string;
+}
+
