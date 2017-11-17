18e974157a4597d636c45e44f3772a2f4975eb77
Unknown <floydwin7@gmail.com>
2017-11-16 23:40:28 -0800

    reorder dbhelper functions

--- CS586MVC/ControllerHelpers/PropertyDataController.DbHelper.cs
+++ CS586MVC/ControllerHelpers/PropertyDataController.DbHelper.cs
@@ -17,129 +17,6 @@ namespace CS586MVC.Controllers
     {      
         public static PropertyMismanagementContext Context { set; private get; }
 
-        public static async Task <AptComplexUnit> AptComplexUnit(int id, bool include = true)
-        {
-            if (include)
-            {
-                return await Context.AptComplexUnits
-                    .Include(e => e.AptComplex)
-                    .Include(e => e.AptUnit)
-                    .FirstOrDefaultAsync(e => e.Id == id);
-            }
-            
-            return await Context.AptComplexUnits.FirstOrDefaultAsync(e => e.Id == id);
-        }
-        
-        public static async Task<IEnumerable<AptComplexUnit>> AllAptComplexUnits(bool include = true)
-        {
-            return include ? 
-                await Context.AptComplexUnits.Include(e => e.AptComplex).ToListAsync() :    
-                await Context.AptComplexUnits.ToListAsync();
-        }
-        
-        public static async Task<Lease> Lease(int id, bool include = true)
-        {
-            if (include)
-            {
-                return await Context.Leases
-                    .Include(p => p.Tenant)
-                    .Include(a => a.Unit)
-                        .ThenInclude(a => a.AptComplex)
-                    .FirstOrDefaultAsync(e => e.Id == id);
-            }
-
-            return await Context.Leases.FirstOrDefaultAsync(e => e.Id == id);
-        }
-        
-        public static async Task<IEnumerable<Lease>> AllLeases(bool include = true)
-        {            
-            if(include)
-            {
-                return await Context.Leases
-                    .Include(p => p.Tenant)
-                    .Include(a => a.Unit)
-                        .ThenInclude(e => e.AptComplex)
-                    .ToListAsync();
-            }
-            
-            return await Context.Leases.ToListAsync();
-        }
-        
-        public static async Task<Person> Person(int id, bool include = true)
-        {
-            if (include)
-            {
-                return await Context.Persons
-                    .Include(p => p.Leases)
-                        .ThenInclude(apt => apt.Unit)
-                            .ThenInclude(unit => unit.AptComplex)
-                    .FirstOrDefaultAsync(p => p.Id == id);
-            }
-
-            return await Context.Persons.FirstOrDefaultAsync(p => p.Id == id);
-        }
-        
-        public static async Task<IEnumerable<Person>> AllPersons(bool include = true)
-        {
-            if (include)
-            {
-                return await Context.Persons
-                    .Include(p => p.Leases)
-                        .ThenInclude(apt => apt.Unit)
-                            .ThenInclude(unit => unit.AptComplex)
-                    .ToListAsync();
-            }
-            
-            return await Context.Persons.ToListAsync();
-        }
-        
-        public static async Task InsertAptComplexUnit(AptComplexUnit acUnit)
-        {
-            bool complex = acUnit.AptComplex != null;
-            bool complexId = acUnit.AptComplexId.HasValue;
-            
-            if (!complex && !complexId)
-            {
-                throw new Exception("an AptComplexUnit object needs an associated AptComplex object!");
-            }
-
-            bool unit = acUnit.AptUnit != null;
-            bool hasUnitId = acUnit.AptUnitId.HasValue;
-            
-            if (!unit && !hasUnitId)
-            {
-                throw new Exception("an AptComplexUnit object needs an associated AptUnit object!");
-            }
-
-            if (complex)
-            {
-                int id = await InsertAptComplex(acUnit.AptComplex);
-                acUnit.AptComplexId = id;
-            }
-
-            if (unit)
-            {
-                int id = await InsertAptUnit(acUnit.AptUnit);
-                acUnit.AptUnitId = id;
-            }
-            
-            Context.AptComplexUnits.Add(acUnit);
-            await Context.SaveChangesAsync();
-        }
-    
-        public static async Task<int> InsertAptUnit(AptUnit unit)
-        {
-            EntityEntry<AptUnit> entry = Context.AptUnits.Add(unit);
-            await Context.SaveChangesAsync();
-            return entry.Entity.Id;
-        }
-    
-        public static async Task InsertPerson(Person p)
-        {
-            Context.Persons.Add(p);
-            await Context.SaveChangesAsync();
-        }
-
         public static async Task<int> InsertAptComplex(AptComplex ac)
         {
             EntityEntry<AptComplex> entry = Context.AptComplexes.Add(ac);
@@ -147,7 +24,7 @@ namespace CS586MVC.Controllers
             return entry.Entity.Id;
         }
 
-public static async Task<AptComplex> AptComplex(int id, bool include = true)
+        public static async Task<AptComplex> AptComplex(int id, bool include = true)
         {
             if (include)
             {
@@ -181,7 +58,7 @@ public static async Task<AptComplex> AptComplex(int id, bool include = true)
             await Context.SaveChangesAsync();
         }
 
-        public static async Task RemoveAptComplexAndDependents(int id)
+        public static async Task RemoveAptComplex(int id)
         {
             //shameful
             AptComplex target = await Context.AptComplexes
@@ -216,11 +93,135 @@ public static async Task<AptComplex> AptComplex(int id, bool include = true)
             Context.Entry(target).State = EntityState.Deleted;
             await Context.SaveChangesAsync();
         }
+
+        public static async Task <AptComplexUnit> AptComplexUnit(int id, bool include = true)
+        {
+            if (include)
+            {
+                return await Context.AptComplexUnits
+                    .Include(e => e.AptComplex)
+                    .Include(e => e.AptUnit)
+                    .FirstOrDefaultAsync(e => e.Id == id);
+            }
+            
+            return await Context.AptComplexUnits.FirstOrDefaultAsync(e => e.Id == id);
+        }
+        
+        public static async Task<IEnumerable<AptComplexUnit>> AllAptComplexUnits(bool include = true)
+        {
+            return include ? 
+                await Context.AptComplexUnits.Include(e => e.AptComplex).ToListAsync() :    
+                await Context.AptComplexUnits.ToListAsync();
+        }
+        
+        public static async Task InsertAptComplexUnit(AptComplexUnit acu)
+        {
+            bool complex = acu.AptComplex != null;
+            bool complexId = acu.AptComplexId.HasValue;
+            
+            if (!complex && !complexId)
+            {
+                throw new Exception("an AptComplexUnit object needs an associated AptComplex object!");
+            }
+
+            bool unit = acu.AptUnit != null;
+            bool hasUnitId = acu.AptUnitId.HasValue;
+            
+            if (!unit && !hasUnitId)
+            {
+                throw new Exception("an AptComplexUnit object needs an associated AptUnit object!");
+            }
+
+            if (complex)
+            {
+                int id = await InsertAptComplex(acu.AptComplex);
+                acu.AptComplexId = id;
+            }
+
+            if (unit)
+            {
+                int id = await InsertAptUnit(acu.AptUnit);
+                acu.AptUnitId = id;
+            }
+            
+            Context.AptComplexUnits.Add(acu);
+            await Context.SaveChangesAsync();
+        }
+    
+        public static async Task<int> InsertAptUnit(AptUnit unit)
+        {
+            EntityEntry<AptUnit> entry = Context.AptUnits.Add(unit);
+            await Context.SaveChangesAsync();
+            return entry.Entity.Id;
+        }
+    
+        public static async Task InsertPerson(Person p)
+        {
+            Context.Persons.Add(p);
+            await Context.SaveChangesAsync();
+        }
+
+        public static async Task<Person> Person(int id, bool include = true)
+        {
+            if (include)
+            {
+                return await Context.Persons
+                    .Include(p => p.Leases)
+                        .ThenInclude(apt => apt.Unit)
+                            .ThenInclude(unit => unit.AptComplex)
+                    .FirstOrDefaultAsync(p => p.Id == id);
+            }
+
+            return await Context.Persons.FirstOrDefaultAsync(p => p.Id == id);
+        }
+        
+        public static async Task<IEnumerable<Person>> AllPersons(bool include = true)
+        {
+            if (include)
+            {
+                return await Context.Persons
+                    .Include(p => p.Leases)
+                        .ThenInclude(apt => apt.Unit)
+                            .ThenInclude(unit => unit.AptComplex)
+                    .ToListAsync();
+            }
+            
+            return await Context.Persons.ToListAsync();
+        }
+        
         public static async Task InsertLease(Lease l)
         {
             Context.Leases.Add(l);
             await Context.SaveChangesAsync();
         }
+        public static async Task<Lease> Lease(int id, bool include = true)
+        {
+            if (include)
+            {
+                return await Context.Leases
+                    .Include(p => p.Tenant)
+                    .Include(a => a.Unit)
+                        .ThenInclude(a => a.AptComplex)
+                    .FirstOrDefaultAsync(e => e.Id == id);
+            }
+
+            return await Context.Leases.FirstOrDefaultAsync(e => e.Id == id);
+        }
+        
+        public static async Task<IEnumerable<Lease>> AllLeases(bool include = true)
+        {            
+            if(include)
+            {
+                return await Context.Leases
+                    .Include(p => p.Tenant)
+                    .Include(a => a.Unit)
+                        .ThenInclude(e => e.AptComplex)
+                    .ToListAsync();
+            }
+            
+            return await Context.Leases.ToListAsync();
+        }
+        
     } 
     }
 }
