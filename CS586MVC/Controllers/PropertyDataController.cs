dc78b9465282d97fd646c89c5b92724143bd5388
Unknown <floydwin7@gmail.com>
2017-11-18 20:40:33 -0800

    function typo

--- CS586MVC/Controllers/PropertyDataController.cs
+++ CS586MVC/Controllers/PropertyDataController.cs
@@ -41,7 +41,7 @@ namespace CS586MVC.Controllers
         public async Task Properties(int id)
         {
             Console.WriteLine($"Deleting AptComplex with ID:{id}");
-            await DbHelper.RemoveAptComplexAndDependents(id);
+            await DbHelper.RemoveAptComplex(id);
         }
 
         [HttpGet]
