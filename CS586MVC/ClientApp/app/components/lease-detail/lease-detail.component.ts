783b93fae8dcfde323cf88ff79bcb3d0b2cab0af
Unknown <floydwin7@gmail.com>
2017-11-22 23:19:53 -0800

    lease details are almost editable

--- CS586MVC/ClientApp/app/components/lease-detail/lease-detail.component.ts
+++ CS586MVC/ClientApp/app/components/lease-detail/lease-detail.component.ts
@@ -1,7 +1,9 @@
 import {Component, OnInit, OnDestroy, Inject} from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Http } from "@angular/http";
-import {Lease} from "../leases/leases.component.interfaces";
+import { Http, Headers } from "@angular/http";
+//import { EditableTableModule } from "ng-editable-table/editable-table/editable-table.module";
+import { Lease } from "../leases/leases.component.interfaces";
+
 
 @Component({
   selector: 'app-lease-detail',
@@ -11,9 +13,10 @@ import {Lease} from "../leases/leases.component.interfaces";
 export class LeaseDetailComponent implements OnInit, OnDestroy {
 
     private leases:Lease[];
+    private trialLease:Lease;
     private sub: any;
     private id:number;
-    private editable:boolean;
+    private editMode:boolean = false;
     
   constructor(private http: Http, @Inject('BASE_URL') private baseUrl: string, private route: ActivatedRoute) {
       
@@ -25,6 +28,9 @@ export class LeaseDetailComponent implements OnInit, OnDestroy {
           
           this.http.get(this.baseUrl + 'propertydata/leases/' + this.id.toString()).subscribe(result => {
               this.leases = result.json() as Lease[];
+              
+              this.trialLease = Object.assign(this.leases[0]);
+              
           }, error => console.error(error));
       });
 
@@ -32,6 +38,29 @@ export class LeaseDetailComponent implements OnInit, OnDestroy {
   
   ngOnDestroy() {
      this.sub.unsubscribe();
+  }
+  
+  toggleEditMode() {
+    this.editMode = !this.editMode;
+  }
+  
+  submit()
+  {
+      this.toggleEditMode();
+      let headers = new Headers();
+      headers.append('Content-Type', 'application/json');
+      
+      let url = this.baseUrl + "/propertydata/leases/" + this.trialLease.id;
+      
+      return this.http
+          .put(url, JSON.stringify(this.trialLease), {headers: headers})
+          .subscribe();
+  }
+  
+  cancel()
+  {
+      this.toggleEditMode();
+      this.trialLease = Object.assign(this.leases[0]);
   }
 
 }
